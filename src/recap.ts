4f894ef52ff9d9e6b109722dc1b18170c7911325
Andrea1904 <andrea.lopez1904@gmail.com>
2022-11-01 21:38:32 -0500

    first commit

---
+++ src/recap.ts
@@ -0,0 +1,19 @@
+const myName = 'Andre';
+const myAge = 12;
+const suma = (a: number, b: number) => {
+  return a + b;
+};
+suma (12,12);
+
+
+class Persona {
+     constructor(private age: number, name:string){}
+
+    getSummary(){
+      return 'my name is ${this.name}, ${this.age};
+
+    }
+}
+
+const andrea= new Persona(15,'andrea');
+andrea.getSummary();
