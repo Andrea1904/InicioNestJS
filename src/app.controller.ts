40e3e7c4d9f93f918d3d7ae89600f7ee3a27c88b
Andrea1904 <andrea.lopez1904@gmail.com>
2022-11-02 23:05:53 -0500

    Modificacion controller:mejores practicas

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,32 +1,11 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
   @Get()
   getHello(): string {
     return 'Ajá perdido';
-  }
-
-  @Get('nuevo')
-  newEndpoint() {
-    return 'Crear nuevo metodo';
-  }
-
-  @Get('/ruta/')
-  hello() {
-    return '/tener en cuenta las rutas/ errores en express';
-  }
-
-  @Get('/product/:productId')
-  getProduct(@Param('productId') productId: string) {
-    return `product ${productId}`;
-  }
-
-  @Get('/categories/:id/products/:productId')
-  getCategory(@Param('productId') productId: string, @Param('id') id: string) {
-    return `product ${productId} and ${id}`;
   }
 }
